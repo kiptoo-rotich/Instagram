d499baa27c573ea684cbbcfb976710358a4c807c
Softonic254 <rotichenoch27@gmail.com>
2021-07-10 19:40:15 +0300

    It adds a registration view function that renders a registration form

--- instapp/views.py
+++ instapp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from .models import Photos,Editor
 import datetime as dt
+from .forms import CreateUserForm
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
     post=Photos.objects.all()
@@ -9,4 +11,11 @@ def home(request):
     date=dt.date.today()
     return render(request, 'main/home.html',{"post":post,"editor":editor,"date":date})
 
-
+def registrationPage(request):
+    form=CreateUserForm()
+    
+    if request.method=='POST':
+        form=CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return render(request,"registration/registration_form.html",{"form":form})
