2716f7d23129b6690a26325ff136fd7337aade3c
Softonic254 <rotichenoch27@gmail.com>
2021-07-10 10:40:22 +0300

    It adds editor as a foreign key in Photos

--- instapp/views.py
+++ instapp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render,redirect
 from django.http import Http404, HttpResponse, HttpResponseRedirect
+from .models import Photos,Editor
 
 def home(request):
-    return render(request, 'main/home.html')
+    post=Photos.objects.all()
+    editor=Editor.objects.all()
+    return render(request, 'main/home.html',{"post":post,"editor":editor})
 
 
