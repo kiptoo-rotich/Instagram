4b2064914dfe51de4beb413bd7b978ae47c2c227
Softonic254 <rotichenoch27@gmail.com>
2021-07-11 20:37:19 +0300

    It adds profile forms to forms file

--- instapp/forms.py
+++ instapp/forms.py
@@ -2,12 +2,30 @@ from django.forms import ModelForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
+from.models import Comment,Profile
 
 class CreateUserForm(UserCreationForm):
     class Meta:
         model=User
         fields=['username', 'email','password1','password2']
 
-class CommentForm():
-    comment=forms.CharField(label="Comment..",max_length=100)
-    
+class CommentForm(forms.ModelForm):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.fields['comment'].widget = forms.TextInput()
+        self.fields['comment'].widget.attrs['placeholder'] ='Add a comment...'
+        
+    class Meta:
+        model=Comment
+        fields = ('comment',)    
+
+class profileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = [ 'profilephoto', 'Bio']
+
+class UserUpdate(forms.ModelForm):
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ['username','email'] 
