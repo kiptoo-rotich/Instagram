2b31a53ea185eaa8c0a413dfb0414bafd935ee0b
Softonic254 <rotichenoch27@gmail.com>
2021-07-12 13:35:47 +0300

    It adds Profile to admin site

--- instapp/admin.py
+++ instapp/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Editor,Photos,tags
+from .models import Profile, Photos,tags
 
 class ArticleAdmin(admin.ModelAdmin):
     filter_horizontal=('tags')
 
-admin.site.register(Editor) 
+admin.site.register(Profile) 
 admin.site.register(Photos) 
 admin.site.register(tags) 
