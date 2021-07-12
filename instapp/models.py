fce4624634daadb38003efc77f9bcfaed6f74861
Softonic254 <rotichenoch27@gmail.com>
2021-07-12 10:52:08 +0300

    It removes Editor class from models

--- instapp/models.py
+++ instapp/models.py
@@ -4,23 +4,12 @@ import datetime as dt
 from django.contrib.auth.models import User
 
 
-class Editor(models.Model):
-    first_name = models.CharField(max_length =30)
-    last_name = models.CharField(max_length =30)
-    email = models.EmailField()
-    phone_number = models.CharField(max_length=10, blank=True)
-
-    def _str_(self):
-        return self.first_name
-
-    def save_editor(self):
-        self.save()
 
 class Photos(models.Model):
     title = models.CharField(max_length=60)
     post=models.TextField(max_length=1000)
     pub_date=models.DateTimeField(auto_now_add=True)
-    editor = models.ForeignKey('Editor',on_delete=models.CASCADE,)
+    profile = models.ForeignKey('Profile',on_delete=models.CASCADE,)
     image=models.ImageField(upload_to='images/', default="Image")
     likes=models.ManyToManyField(User,related_name='post_like')
     
